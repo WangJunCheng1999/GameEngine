fa60e660feb45492188508cdbcdf1e820d7c598f
wangjuncheng <wjc14778@163.com>
2022-09-17 01:06:21 +0800

    modify bat command

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,2 +1,7 @@
+del /f /s .\Sandbox\Sandbox.vcxproj
+del /f /s .\Sandbox\Sandbox.vcxproj.user
+del /f /s .\GameEngine\GameEngine
+del /f /s .\GameEngine\GameEngine.vcxproj
+
 call vendor\bin\premake\premake5.exe vs2017
 PAUSE
